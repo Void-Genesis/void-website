14b0c3e6cf9eeb581d2967de1d003f89fe3c8cb1
rodrigomonney <rodrigomonney@gmail.com>
2024-01-30 18:19:20 -0300

    update: change responsiveness for 1440px screens

---
+++ src/components/ui/TeamCard.tsx
@@ -0,0 +1,19 @@
+interface TeamCardProps {
+  name: string;
+  role: string;
+  imageSrc: string;
+  altText: string;
+}
+
+export default function TeamCard({ name, role, imageSrc, altText }: TeamCardProps) {
+  return (
+    <div className="px-5 pt-5 pb-8 rounded border border-void-grey-900 ">
+      <div className="flex flex-col items-center justify-center text-center">
+        <img className="rounded-full" src={imageSrc} alt={altText} />
+
+        <h1 className="pt-3 text-void-white font-sora font-semibold text-xl leading-[120%]">{name}</h1>
+        <p className="pt-6 text-void-white-400 font-heebo text-lg font-medium tracking-[0.18px] leading-[120%]">{role}</p>
+      </div>
+    </div>
+  );
+}
